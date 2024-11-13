d86bd0ed8ab9365d7b8bb99aa5d7369a02d7c48d
com12300000 <com12300000@gmail.com>
2024-11-13 12:03:39 +0900

    個々の作品の表示をコンポーネント化

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,8 +1,33 @@
+import React from 'react'
+import matrixCalculation from '../assets/img/行列計算.png'
+
+interface WorkItemProps{
+    img: string;
+    title: string;
+}
+
+const WorksItem:React.FC<WorkItemProps> = ({img, title}) => {
+    return (
+        <div className="work-item">
+            <div className='thumbnail'>
+                <img src={img} />
+            </div>
+            <div className='work-content'>
+                <h3>{title}</h3>
+            </div>
+        </div>
+    )
+}
+
+
 const Works = () => {
   return (
     <div className="works">
         <div className="section-title">
             <h2>Works</h2>
+        </div>
+        <div className='items'>
+            <WorksItem img={matrixCalculation} title='行列計算アプリ' />
         </div>
     </div>
   )
