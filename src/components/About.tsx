7c591eeb1698e5a086d08553213efcb1c07efe95
com12300000 <com12300000@gmail.com>
2024-11-13 09:11:53 +0900

    自己紹介文作成

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,23 @@
+import aboutImg from '../assets/img/IMG_7007.jpeg'
+
 const About = () => {
   return (
     <div className="about">
       <div className="section-title">
         <h2>about</h2>
       </div>
-      aboutページ
+      <div className='main'>
+        <div className='img'>
+          <img src={aboutImg} />
+        </div>
+        <div className='text-area'>
+          <div className='text'>
+            大学卒業後、公立中学校の数学科教員として6年間務めました。
+            業務の中で、プログラミング言語に出会いエンジニアを志しました。
+            フロントエンドエンジニアを目指し、HTML,CSS,JavaScriptをはじめ、React,TypeScriptでの開発を通してスキル向上に努めています。
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
