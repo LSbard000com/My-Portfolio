1e6d1d55f04f9300630d1242c1ce4416fc27e3e4
com12300000 <com12300000@gmail.com>
2024-11-14 22:42:50 +0900

    コンタクトページ作成開始

---
+++ src/components/Contact.tsx
@@ -0,0 +1,12 @@
+const Contact = () => {
+  return (
+    <div className="contact">
+        <div className="section-title">
+            <h2>Contact</h2>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Contact
