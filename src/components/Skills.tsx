fe3bef1b298d90dca04d7ae8bc48358abc98bb1b
com12300000 <com12300000@gmail.com>
2024-11-14 13:09:45 +0900

    タイトル作成

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,10 @@
 const Skills = () => {
   return (
-    <div>
-      マイスキルです。
+    <div className="my-skills">
+        <div className="section-title">
+            <h2>Skills</h2>
+        </div>
+      
     </div>
   )
 }
