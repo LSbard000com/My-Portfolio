df37c1e1727ba3c23c2e026c18445eb20ddab996
com12300000 <com12300000@gmail.com>
2024-11-14 18:50:41 +0900

    サイドバーにリックを追加

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -6,20 +6,37 @@ const SideBar = () => {
 
   return (
     <div className={`sidebar ${open ? 'open' : ''}`}>
+
+        {/* トグルボタン */}
         <div className='nav-btn' onClick={()=>setOpen(!open)} >
             <span></span>
             <span></span>
             <span></span>
         </div>
-        <div>
-            <nav className='navigation'>
+
+        {/* ナビゲーション */}
+        <div className='navigation'>
+            <nav className='nav'>
                 <ul>
                     <li onClick={()=>setOpen(!open)}><a href='#top'>Top</a></li>
                     <li onClick={()=>setOpen(!open)}><a href='#about'>About</a></li>
                     <li onClick={()=>setOpen(!open)}><a href='#works'>Works</a></li>
                     <li onClick={()=>setOpen(!open)}><a href='#skills'>Skills</a></li>
+                    <li onClick={()=>setOpen(!open)}><a href='#contact'>Contact</a></li>
                 </ul>
             </nav>
+            <div className='links'>
+                <ul>
+                    <li>
+                        <i className="fa-brands fa-github"></i>
+                        <a href='https://github.com/LSbard000com'>GitHub</a>
+                    </li>
+                    <li>
+                        <i className="fa-solid fa-pen-to-square"></i>
+                        <a href='https://note.com/ready_dietes6250/'>note</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
   )
