0dc3fe40160cf4d0ef7713d2dfe871870b72e21a
com12300000 <com12300000@gmail.com>
2024-11-15 01:06:32 +0900

    フッター完成

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer'>
-      
+      <div className='copyright'>
+        <div>&copy; Masato Komukai Portfolio</div>
+      </div>
+      <div className='jump-btn'>
+        <a href='#top'>
+            <span></span>
+            <span></span>
+        </a>
+      </div>
     </div>
   )
 }
