202b0ec7ad4438b46649825ef50c4db12cca8515
com12300000 <com12300000@gmail.com>
2024-11-15 21:49:27 +0900

    タイムライン作成中

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
@@ -6,28 +5,71 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 
 const TimeLine = () => {
+    // タイムラインのデータ
+    const timelineElements = [
+        {
+            id: 'event-2019-4',
+            date: '2024年 4月'
+
+        }
+    ]
+
+
     return (
         <VerticalTimeline>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            contentStyle={{ background: "#6ca0dc", color: "#fff" }}
-            contentArrowStyle={{ borderRight: "7px solid #6ca0dc" }}
-            date="2015年"
-            iconStyle={{ background: "#6ca0dc", color: "#fff" }}
+            contentStyle={{ background: "rgba(255, 255, 255, 0.2)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid rgba(255, 255, 255, 0.2)" }}
+            date="2019年 4月"
+            iconStyle={{ background: "#193278", color: "#fff" }}
           >
-            <h3 className="vertical-timeline-element-title">市役所勤務開始</h3>
-            <p>市民サービス部門に配属され、地域活性化プロジェクトに従事。</p>
+            <h3 className="vertical-timeline-element-title">公立中学校に配属</h3>
+            <p>初任から担任を務める</p>
           </VerticalTimelineElement>
     
           <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            contentStyle={{ background: "#f9c74f", color: "#fff" }}
-            contentArrowStyle={{ borderRight: "7px solid #f9c74f" }}
-            date="2023年"
-            iconStyle={{ background: "#f9c74f", color: "#fff" }}
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgba(255, 255, 255, 0.2)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid rgba(255, 255, 255, 0.2)" }}
+            date="2019年 4月"
+            iconStyle={{ background: "#193278", color: "#fff" }}
           >
-            <h3 className="vertical-timeline-element-title">ReactとFirebaseを学ぶ</h3>
-            <p>個人プロジェクトを通じて、モダンウェブ開発に取り組む。</p>
+            <h3 className="vertical-timeline-element-title">公立中学校に配属</h3>
+            <p>初任から担任を務める</p>
+          </VerticalTimelineElement>
+
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgba(255, 255, 255, 0.2)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid rgba(255, 255, 255, 0.2)" }}
+            date="2019年 4月"
+            iconStyle={{ background: "#193278", color: "#fff" }}
+          >
+            <h3 className="vertical-timeline-element-title">公立中学校に配属</h3>
+            <p>初任から担任を務める</p>
+          </VerticalTimelineElement>
+
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgba(255, 255, 255, 0.2)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid rgba(255, 255, 255, 0.2)" }}
+            date="2019年 4月"
+            iconStyle={{ background: "#193278", color: "#fff" }}
+          >
+            <h3 className="vertical-timeline-element-title">公立中学校に配属</h3>
+            <p>初任から担任を務める</p>
+          </VerticalTimelineElement>
+
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgba(255, 255, 255, 0.2)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid rgba(255, 255, 255, 0.2)" }}
+            date="2019年 4月"
+            iconStyle={{ background: "#193278", color: "#fff" }}
+          >
+            <h3 className="vertical-timeline-element-title">公立中学校に配属</h3>
+            <p>初任から担任を務める</p>
           </VerticalTimelineElement>
         </VerticalTimeline>
       );
