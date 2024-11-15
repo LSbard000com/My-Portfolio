7147db8b5d7874a3f732bf386cd7a01bf6d0f569
com12300000 <com12300000@gmail.com>
2024-11-15 16:20:38 +0900

    プロフィールを作成し、挿入画像、自己紹介文削除

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,5 @@
 import aboutImg from '../assets/img/IMG_7007.jpeg'
+import myPhoto from '../assets/img/自己紹介画像.jpg'
 import Title from './Title'
 
 const About = () => {
@@ -6,17 +7,27 @@ const About = () => {
     <div id='about' className="about">
       <Title 
         title='About'
-        text=''
+        text='はじめましてどうぞごゆくっりご覧ください'
        />
       <div className='main'>
-        <div className='img'>
-          <img src={aboutImg} />
-        </div>
-        <div className='text-area'>
-          <div className='text'>
-            大学卒業後、公立中学校の数学科教員として6年間務めました。
-            業務の中で、プログラミング言語に出会いエンジニアを志しました。
-            フロントエンドエンジニアを目指し、HTML,CSS,JavaScriptをはじめ、React,TypeScriptでの開発を通してスキル向上に努めています。
+        <div className='profile'>
+          <div className='photo'>
+            <img src={myPhoto} alt='自己紹介写真' />
+          </div>
+          <div className='info'>
+            <table>
+              <tbody>
+                <tr>
+                  <th>名前：</th><td>小向 真人（こむかい まさと）</td>
+                </tr>
+                <tr>
+                  <th>生年月日：</th><td>1997年 1月 23日</td>
+                </tr>
+                <tr>
+                  <th>趣味：</th><td>バドミントン、数学</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
