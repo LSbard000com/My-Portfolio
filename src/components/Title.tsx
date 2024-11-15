0a016b94fb1904253e5a165ad9afcf384e5373c6
com12300000 <com12300000@gmail.com>
2024-11-15 12:15:08 +0900

    セクションタイトルをコンポーネント化

---
+++ src/components/Title.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type TitleProps = {
+    title: string;
+    text: string;
+}
+
+const Title:React.FC<TitleProps> = ({}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Title
