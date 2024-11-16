66d9d4fdca5dadf6c1d4e5277e0d6422e8aedb52
com12300000 <com12300000@gmail.com>
2024-11-16 12:46:56 +0900

    タイムライン変更

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -11,33 +11,38 @@ const TimeLine = () => {
         {
             id: 'event-2019-4',
             title: '2019 - April',
-            content: '公立中学校に数学科教員として採用。初任から担任を務める。'
+            content: '公立中学校に数学科教員として採用。初任から担任を務める。',
+            icon: <i className="fa-solid fa-play"></i>
         },
         {
             id: 'event-2020-2',
             title: '2020 - February',
-            content: 'コロナの影響で一斉休校に。春休み開けからは在宅勤務になり、オンライン授業を取り入れる中で授業動画作成を担当する。'
+            content: 'コロナの影響で一斉休校に。春休み開けからは在宅勤務になり、オンライン授業を取り入れる中で授業動画作成を担当する。',
+            icon: <i className="fa-solid fa-exclamation"></i>
         },
         {
             id: 'event-2023-4',
             title: '2023 - April',
-            content: '今年度の行事のリーダーになる。教員の仕事分担や渉外を通して、マネジメントの大変さを学ぶ。行事が実施される10月まで大忙しだったが、とても良い経験となった。'
+            content: '今年度の行事のリーダーになる。教員の仕事分担や渉外を通して、マネジメントの大変さを学ぶ。行事が実施される10月まで大忙しだったが、とても良い経験となった。',
+            icon: <i className="fa-solid fa-person-running"></i>
         },
         {
             id: 'event-2023-8',
             title: '2023 - August',
-            content: 'アナログな作業を効率化させようとVBAを学習しプログラミングに出会う。プログラミング言語の便利さと複雑さゆえの楽しさに気づき興味を持ち始める。'
+            content: 'アナログな作業を効率化させようとVBAを学習しプログラミングに出会う。プログラミング言語の便利さと、複雑さゆえの楽しさに気づき興味を持ち始める。',
+            icon: <i className="fa-solid fa-lightbulb"></i>
         },
         {
             id: 'event-2024-7',
             title: '2024 - July',
-            content: 'エンジニアになることを決意し本格的にプログラミングの学習を始める。Webエンジニアとしてのキャリアを軸に、HTML、CSS、JavaScriptの学習をする。現在はRactでの開発、Next.jsの学習に没頭中。'
+            content: 'エンジニアになることを決意し本格的にプログラミングの学習を始める。Webエンジニアとしてのキャリアを軸に、HTML、CSS、JavaScriptの学習をする。現在はRactでの開発、Next.jsの学習に没頭中。',
+            icon: <i className="fa-solid fa-laptop"></i>
         },
     ]
 
     // 画面に表示されたイベントを監視
     return (
-        <VerticalTimeline animate={false}>
+        <VerticalTimeline animate={false} lineColor="#005070">
             {timelineElements.map((element) => (
                 <InView key={element.id} threshold={0.5}>
                     {({ref, inView}) => (
@@ -53,7 +58,12 @@ const TimeLine = () => {
                                 transition: "all 0.5s ease-out",
                             }}
                             contentArrowStyle={{ borderRight: "7px solid rgba(0, 80, 112, 0.9)" }}
-                            iconStyle={{ background: "#e5cf6e", color: "#fff" }}         
+                            iconStyle={{ background: "#193278", color: "#fff"}}   
+                            icon={
+                                <span style={{height: '100%', display: 'flex' , justifyContent: 'center', alignItems: 'center', fontSize: '20px'}}>
+                                    {element.icon}
+                                </span>
+                            }      
                         >
                             <h2>{element.title}</h2>
                             <p>{element.content}</p>
