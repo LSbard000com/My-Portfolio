98288666ec2f8145854968a3187c78b3ea9ee3f1
com12300000 <com12300000@gmail.com>
2024-11-17 00:38:34 +0900

    トップ画面のレイアウト完成

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,20 +1,17 @@
-import { useState } from 'react'
 import topImage from '../assets/img/IMG_7005.jpeg'
 import { useInView } from 'react-intersection-observer'
 import { motion } from 'framer-motion'
 
 const top = () => {
-    // トグルボタンをクリックするたびにクラスを付与したり外したりする
-    const [open, setOpen] = useState<boolean>(false)
-
-
 
     // 要素の表示を監視
     const { ref, inView } = useInView()
 
   return (
-    <div id='top' className={`top ${open ? 'open' : ''}`}>
+    <div id='top' className='top'>
         <div className='title' ref={ref}>
+
+            {/* タイトル画像 */}
             <motion.img 
                 src={topImage} 
                 alt='top image'
@@ -22,42 +19,46 @@ const top = () => {
                 animate={inView ? { opacity: 1, boxShadow: '0px 0px 20px #aaa'} : {}}
                 transition={{ duration: 5 }}
             />
-            <div className='title-text name'>
-                <motion.h2 
-                    className='first'
-                    initial={{opacity: 0, x: -50}}
-                    animate={inView ? { opacity: 1, x: 0 } : {}}
-                    transition={{ duration: 1, delay: 1.5 }}
-                >
-                    Masato
-                </motion.h2>
-                <motion.h2 
-                    className='last'
-                    initial={{opacity: 0, x: -50}}
-                    animate={inView ? { opacity: 1, x: 0 } : {}}
-                    transition={{ duration: 1 , delay: 2}}
-                >
-                    Komukai
-                </motion.h2>
-            </div>
-            <div className='title-text portfolio'>
-                <motion.h2
-                    initial={{opacity: 0, x: -50}}
-                    animate={inView ? { opacity: 1, x: 0 } : {}}
-                    transition={{ duration: 1 , delay: 2.5}}
-                >
-                    Portfolio
-                </motion.h2>
-            </div>
-        </div>
-        <div className='nav-btn' onClick={()=>setOpen(!open)} >
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-        <div>
-            <nav className='navigation'>
 
+            {/* タイトルテキスト */}
+            <div className='title-texts'>
+                <div className='title-text name'>
+                    <motion.h2 
+                        className='first'
+                        initial={{opacity: 0, x: -50}}
+                        animate={inView ? { opacity: 1, x: 0 } : {}}
+                        transition={{ duration: 1, delay: 1.5 }}
+                    >
+                        Masato
+                    </motion.h2>
+                    <motion.h2 
+                        className='last'
+                        initial={{opacity: 0, x: -50}}
+                        animate={inView ? { opacity: 1, x: 0 } : {}}
+                        transition={{ duration: 1 , delay: 2}}
+                    >
+                        Komukai
+                    </motion.h2>
+                </div>
+                <div className='title-text portfolio'>
+                    <motion.h2
+                        initial={{opacity: 0, x: -50}}
+                        animate={inView ? { opacity: 1, x: 0 } : {}}
+                        transition={{ duration: 1 , delay: 2.5}}
+                    >
+                        Portfolio
+                    </motion.h2>
+                </div>
+            </div>
+        </div>
+        <div className='top-navigation'>
+            <nav>
+                <ul>
+                    <li><a href='#about'>About</a></li>
+                    <li><a href='#works'>Works</a></li>
+                    <li><a href='#skills'>Skills</a></li>
+                    <li><a href='#contact'>Contact</a></li>
+                </ul>
             </nav>
         </div>
     </div>
