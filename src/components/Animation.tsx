7da81a28f825df5b6476d7fac932c51cc9aec92b
com12300000 <com12300000@gmail.com>
2024-11-19 22:00:01 +0900

    タイトルコンポーネントのアニメーションを左右のフェードインの指定ができるように作成

--- src/components/Animation.tsx
+++ src/components/Animation.tsx
@@ -1,6 +1,13 @@
 export const FadeInRight = {
-    inital: { opacity: 0, x: 50},
-    animate: { opactiy: 1, x: 0}, 
-    transition : { duration : 0.8},
+    inital: { opacity: 0, x: 200},
+    animate: { opacity: 1, x: 0}, 
+    transition : { duration : 1},
     exit: { opacity: 0, x: 50}
+}
+
+export const FadeInLeft = {
+    inital: { opacity: 0, x: -200},
+    animate: { opacity: 1, x: 0}, 
+    transition : { duration : 1},
+    exit: { opacity: 0, x: -50}
 }
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,25 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useInView } from 'react-intersection-observer'
 import { motion } from 'framer-motion'
+import { FadeInLeft, FadeInRight } from './Animation'
 
 
 type TitleProps = {
     title: string;
     text: string;
+    animationType: "right" | "left"
 }
 
-const Title:React.FC<TitleProps> = ({title, text}) => {
+const Title:React.FC<TitleProps> = ({title, text, animationType}) => {
   // 要素の表示を監視
   const { ref, inView } = useInView()
+
+
+  // アニメーションの種類を指定
+  const [animation, setAnimation] = useState<any>({})
+  useEffect(() => {
+    if(animationType === "right"){
+      setAnimation(FadeInRight)
+    } else if (animationType === "left"){
+      setAnimation(FadeInLeft)
+    }
+  },[animationType])
 
   return (
     <>
     <div className="section-title" ref={ref}>
         <motion.h2
-          initial={{opacity: 0 }}
-          animate={inView ? { opacity: 1 } : {}}
-          transition={{ duration: 1, delay: 0.5 }}
-          exit={{opacity: 0}}
+          initial={animation.inital}
+          animate={inView ? animation.animate : {}}
+          transition={animation.transition}
+          exit={animation.exit}
         >{title}</motion.h2>
     </div>
     <div className='section-text' ref={ref}>
