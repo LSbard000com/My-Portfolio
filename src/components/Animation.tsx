e9c26b2886e2e073c5b98ac2431bcd53834a76f9
com12300000 <com12300000@gmail.com>
2024-11-20 00:18:25 +0900

    アニメーション修正

--- src/components/Animation.tsx
+++ src/components/Animation.tsx
@@ -1,13 +1,12 @@
 export const FadeInRight = {
-    inital: { opacity: 0, x: 200},
+    inital: { opacity: 0, x: 50, },
     animate: { opacity: 1, x: 0}, 
-    transition : { duration : 1},
-    exit: { opacity: 0, x: 50}
+    transition : { duration : 0.5, delay: 0.5},
 }
 
 export const FadeInLeft = {
-    inital: { opacity: 0, x: -200},
+    inital: { opacity: 0, x: -50},
     animate: { opacity: 1, x: 0}, 
-    transition : { duration : 1},
-    exit: { opacity: 0, x: -50}
+    transition : { duration : 0.5, delay: 0.5},
+
 }
